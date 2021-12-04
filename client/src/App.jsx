658944292653dfe27480376a12f05f37ab4994c8
Irina <ioblomova13@gmail.com>
2021-12-04 22:54:43 +0200

    fix useEffect bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,24 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import actions from './store/actions';
 import Tickers from './services/tickers';
 
 const App = () => {
     const dispatch = useDispatch();
-
+    const tickers = useSelector((state)=> state.tickerReducer.tickers);
+    console.log(tickers)
     const setTickersData = (data) => {
         dispatch(actions.loadTickers(data));
     }
 
     useEffect(() => {
         new Tickers(process.env.REACT_APP_TICKER_SERVER, setTickersData);
-    });
+    },[]);
 
     return (
         <div className="App">
-            <h1>My application</h1>
+
+            <h1>Go</h1>
         </div>
     );
 }
